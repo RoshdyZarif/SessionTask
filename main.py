9e9fe34acde9b0090eb4505a36bce53037c40fa7
Nada-Mostafa31 <nada.mostafa@ejust.edu.eg>
2025-09-14 22:54:45 +0300

    Multiplication function and main function

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 def multiply(a, b):
     return a * b
 
+
 def main():
     print("Simple Calculator")
     print("Choose an operation:")
     print("1. Addition")
     print("2. Subtraction")
     print("3. Multiplication")
+    print("4. Division")
 
-    choice = input("Enter choice (1/2/3): ")
+    choice = input("Enter choice (1/2/3/4): ")
 
     try:
         x = float(input("Enter first number: "))
         y = float(input("Enter second number: "))
     except ValueError:
-        print("❌ Please enter valid numbers!")
+        print("Please enter valid numbers!")
         return
 
     if choice == "1":
@@ -23,8 +25,10 @@ def main():
         print(f"{x} - {y} = {subtract(x, y)}")
     elif choice == "3":
         print(f"{x} * {y} = {multiply(x, y)}")
+    elif choice == "4":
+        print(f"{x} / {y} = {divide(x, y)}")
     else:
-        print("❌ Invalid choice")
+        print(" Invalid choice")
 
 if __name__ == "__main__":
     main()
