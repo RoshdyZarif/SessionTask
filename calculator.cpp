82703ca79f41b6888c576df7c7174e973e5625fd
omarwalid-ghosty <omar.walid.1220053@gmail.com>
2025-09-14 22:28:23 +0300

    Addition

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Addition function
+int add(int a, int b) {
+    return a + b;
+}
+
+// Example usage (for testing)
+int main() {
+    int x = 5, y = 7;
+    cout << "Sum = " << add(x, y) << endl;
+    return 0;
+}
